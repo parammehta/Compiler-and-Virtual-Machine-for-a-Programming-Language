41c5c391bcef8095b346cabde979f8e3548d40c5
anujthula <athula@asu.edu>
2017-04-29 14:28:27 -0700

    Added instructions for generating Intermediate Code and Output

--- Compiler/src/de/letsbuildacompiler/compiler/Main.java
+++ Compiler/src/de/letsbuildacompiler/compiler/Main.java
@@ -19,9 +19,17 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		tempDir = Files.createTempDirectory("outputTest");
-		ANTLRInputStream input = new ANTLRFileStream("code.demo");
-		//System.out.println(compile(input));
-		System.out.println(compileAndRun(input));
+		ANTLRInputStream input = new ANTLRFileStream("./Compiler/code.demo");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Instructions\nPress 1 for Intermediate Code\nPress 2 for Output");
+		int inputfromuser = s.nextInt();
+		if (inputfromuser == 1) {
+			System.out.println(compile(input));
+		} else if (inputfromuser == 2) {
+			System.out.println(compileAndRun(input));
+		} else {
+			System.out.println("Invalid choice");
+		}
 	}
 	
 	private static String compileAndRun(ANTLRInputStream input) throws Exception {
